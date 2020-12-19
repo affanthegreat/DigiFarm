a23f0f8204206c7f8af46e42d6c13f5be0117ab2
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2020-12-20 02:49:34 +0530

    Added info page

--- lib/general/infoPage.dart
+++ lib/general/infoPage.dart
@@ -30,24 +30,34 @@ class _informationPageState extends State<informationPage> {
     final width = MediaQuery.of(context).size.width;
 
 
-   Container heading(String x){
-      return Container(
-          decoration: BoxDecoration(
-            color: Colors.grey.shade200,
-            borderRadius: BorderRadius.circular(20)
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 40,right: 40,top: 20,bottom: 20),
-            child: Text(x.toUpperCase(),style: TextStyle(fontSize:20,fontFamily: "Poppins",fontWeight: FontWeight.bold)),
-          ));
-    }
-  Padding Paragraph(String x){
+Padding heading(String x) {
       return Padding(
-        padding: const EdgeInsets.only(left:20.0,right: 20,top: 10,bottom: 10),
-        child: Text(x,style: TextStyle(fontSize:16,fontFamily: "Poppins",fontWeight: FontWeight.w400)),
+        padding: const EdgeInsets.only(left: 20, right: 40, top: 20, bottom: 5),
+        child: Row(
+          children: [
+            Container(
+                width: width * 0.8,
+                child: Text(x,
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.bold))),
+          ],
+        ),
       );
     }
 
+    Padding Paragraph(String x) {
+      return Padding(
+        padding:
+            const EdgeInsets.only(left: 20.0, right: 20, top: 5, bottom: 10),
+        child: Text(x,
+            style: TextStyle(
+                fontSize: 16,
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.w400)),
+      );
+    }
 
     return Scaffold(
 
@@ -57,18 +67,29 @@ class _informationPageState extends State<informationPage> {
             Container(
               height: height,
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topRight: Radius.circular(height * 0.03),topLeft: Radius.circular(height * 0.03))
+                  color: Colors.grey.shade50,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(height * 0.03),
+                      topLeft: Radius.circular(height * 0.03))
               ),
               child: ListView.builder(
 
                 itemCount: widget.Paragraphs.length,
                 itemBuilder: (context,index){
                   return Column(
-                    children:[
-                    heading(widget.titles[index]),
-                    Paragraph(widget.Paragraphs[index])
-                 ]);
+                    children: [
+                      heading(widget.titles[index]),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0, right: 30),
+                        child: Divider(
+                          thickness: 1, color: Colors.greenAccent,),
+                      ),
+                      Paragraph(widget.Paragraphs[index]),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Divider(thickness: 0.5, color: Colors.black,),
+                      ),
+                    ]);
                 },
               ),
             )
